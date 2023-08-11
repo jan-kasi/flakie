ed87c51f1d0526350dde050356311baf88bd93e9
jan-kasi <77466026+jan-kasi@users.noreply.github.com>
2023-08-11 03:02:19 +0100

    fix themes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -37,7 +37,6 @@
   # packages to install for user #
   # >>>>>>>>>>>>>><<<<<<<<<<<<<< #
   home.packages = with pkgs; [
-    fzf
     fd
     exa
     grc
@@ -59,7 +58,7 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
-    theme = "Dracula";
+    theme = "Catppuccin-Mocha";
     font.name = "Meslo LGS NF";
     settings = {
       allow_remote_control = "yes";
@@ -70,8 +69,23 @@
       mouse_hide_wait = "3";
       inactive_text_alpha = "0.7";
       window_padding_width = "1";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
     };
   };
+
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "jan-kasi";
+    userEmail = "77466026+jan-kasi@users.noreply.github.com";
+  };
+
+  # Firefox
+  programs.firefox = {
+    enable = true;
+  };
+
   # Fish shell
   programs.fish = {
     enable = true;
@@ -87,32 +101,28 @@
       { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
       { name = "done"; src = pkgs.fishPlugins.done.src; }
       { name = "sponge"; src = pkgs.fishPlugins.sponge.src; }
-      { name = "colored-man-pages"; src = pkgs.fishPlugins.colored-man-pages.src; }
       { name = "pisces"; src = pkgs.fishPlugins.pisces.src; }
-    # { name = "tide"; src = pkgs.fishPlugins.tide.src; }
       { name = "foreign-env"; src = pkgs.fishPlugins.foreign-env.src; }  
 
       # Manually packaging and enabling a plugin
-      { name = "dracula";
-        src = pkgs.fetchFromGitHub {
-	  owner = "dracula";
-	  repo = "fish";
-	  # You can find the latest git commit hash with
-	  # '$ git ls-remote $URL HEAD'
-	  rev = "269cd7d76d5104fdc2721db7b8848f6224bdf554";
-	  # To update this after changing rev/url/etc empty 'sha256 = "";'
-	  # Replace with sha "got" by error output (just copy paste all of it)
-	  sha256 = "sha256-Hyq4EfSmWmxwCYhp3O8agr7VWFAflcUe8BUKh50fNfY=";
-        };
-      }
-      { name = "catppuccin";
-        src = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "fish";
-          rev = "91e6d6721362be05a5c62e235ed8517d90c567c9";
-          hash = "sha256-l9V7YMfJWhKDL65dNbxaddhaM6GJ0CFZ6z+4R6MJwBA=";
-	};
-      }
+      
+      # Catppuccin with following perhaps?
+      # { name = "Catppuccin-Mocha";
+      #  src = builtins.readFile();
+      # }
+      
+      #{ name = "dracula";
+      #  src = pkgs.fetchFromGitHub {
+      #	  owner = "dracula";
+      #	  repo = "fish";
+      #	  # You can find the latest git commit hash with
+      #   # '$ git ls-remote $URL HEAD'
+      #	  rev = "269cd7d76d5104fdc2721db7b8848f6224bdf554";
+      #	  # To update this after changing rev/url/etc empty 'sha256 = "";'
+      #	  # Replace with sha "got" by error output (just copy paste all of it)
+      #	  sha256 = "sha256-Hyq4EfSmWmxwCYhp3O8agr7VWFAflcUe8BUKh50fNfY=";
+      #  };
+      # }
     ];
   };
 
@@ -137,28 +147,46 @@
   # Bat (cat clone)
   programs.bat = {
     enable = true;
+    config.theme = "Catppuccin-Mocha";
+    config.pager = "less -FR";
     themes = {
       dracula = builtins.readFile (pkgs.fetchFromGitHub {
         owner = "dracula";
-        repo = "sublime"; # Bat uses sublime syntax for its themes
-        rev = "c5de15a0ad654a2c7d8f086ae67c2c77fda07c5f";
-        sha256 = "sha256-m/MHz4phd3WR56I5jfi4hMXnFf4L4iXVpMFwtd0L0XE=";
-      } + "/Dracula.tmTheme");
+        repo = "sublime"; # Bat uses sublime syntax for its themes 
+	rev = "c5de15a0ad654a2c7d8f086ae67c2c77fda07c5f"; 
+	sha256 = "sha256-m/MHz4phd3WR56I5jfi4hMXnFf4L4iXVpMFwtd0L0XE="; 
+	}+ "/Dracula.tmTheme");
+      Catppuccin-Mocha = builtins.readFile (pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+	repo = "bat";
+        rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+        sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";	
+	}+ "/Catppuccin-mocha.tmTheme");
     };
-    config.theme = "Dracula";
-    config.pager = "less -FR";
   };
 
-  # Git
-  programs.git = {
+  # fzf
+  programs.fzf = {
     enable = true;
-    userName = "jan-kasi";
-    userEmail = "77466026+jan-kasi@users.noreply.github.com";
-  };
-
-  # Firefox
-  programs.firefox = {
-    enable = true;
+    enableFishIntegration = false;
+    defaultOptions = [
+      "--ansi"
+      "--preview-window 'right:60%'"
+      "--preview 'bat --color=always --style=header,grid --line-range :300 {}'"];
+    colors = {
+      fg = "#f8f8f2";
+      bg = "#282a36";
+      hl = "#bd93f9";
+      "fg+" = "#f8f8f2";
+      "bg+" = "#44475a";
+      "hl+" = "#bd93f9";
+      info = "#ffb86c";
+      prompt = "#50fa7b";
+      pointer = "#ff79c6";
+      marker = "#ff79c6";
+      spinner = "#ffb86c";
+      header = "#6272a4";
+    };
   };
 
   ## You can import other home-manager modules here ##
