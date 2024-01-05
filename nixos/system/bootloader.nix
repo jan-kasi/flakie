68e1f199736a1e82d3916dd7e7b92d41edf9b684
jan-kasi <77466026+jan-kasi@users.noreply.github.com>
2024-01-05 23:37:43 +0000

    perhaps finally fixing the freeze, crash, input/output i/o error things which seemed to be making disks read only, might have been an ssd apst problem

--- nixos/system/bootloader.nix
+++ nixos/system/bootloader.nix
@@ -5,7 +5,16 @@
     # For gamepad controller?
     # initrd.kernelModules = [  ];
 
+    # use the latest kernel
     kernelPackages = pkgs.linuxPackages_latest;
+
+
+    # potential reason and solution for freezes/crashes
+    # https://discourse.nixos.org/t/why-does-my-root-mount-suddenly-remount-as-read-only-after-a-while/33595/11
+    kernelParams = [
+      "nvme_core.default_ps_max_latency_us=0"
+      "pcie_aspm=off"
+    ];
 
     # Should probably manually disable this
     # swraid.enable = false; # Default: "`true` if stateVersion is older than 23.11"
