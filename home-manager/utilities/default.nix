74de295e2df04f3c52ca4c56cf5b8a7108fb67fe
jan-kasi <77466026+jan-kasi@users.noreply.github.com>
2024-01-08 15:15:54 +0000

    tidy utilities, add thefuck command corrector

--- home-manager/utilities/default.nix
+++ home-manager/utilities/default.nix
@@ -1,10 +1,35 @@
 {
   imports = [
-    ./git.nix
     ./mpd.nix
     ./beets.nix
     ./bat.nix
-    ./fzf.nix
-    # ./wireplumber.nix
   ];
+
+  programs.git = {
+    userName = "jan-kasi";
+    userEmail = "77466026+jan-kasi@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+        editor = "hx";
+      };
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = false;
+    defaultOptions = [
+      "--ansi"
+      "--border"
+      "--layout=reverse"
+      "--cycle"
+      "--height=90%"
+      "--marker=\"*\""
+      "--preview-window=wrap"
+      "--color=fg:white,bg:black,hl:red,fg+:white,hl+:red,info:yellow,prompt:blue,pointer:magenta,marker:magenta,spinner:green,header:blue,border:white"
+    ];
+  };
+
+  programs.thefuck.enable = true;
 }
