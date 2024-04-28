6f929fe8eb2c7b0040ccdd83068568171007bdb0
jan-kasi <77466026+jan-kasi@users.noreply.github.com>
2024-04-28 09:16:34 +0100

    add fennel to helix

--- home-manager/terminal/helix.nix
+++ home-manager/terminal/helix.nix
@@ -2,7 +2,12 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
-
+    extraPackages = [
+      pkgs.marksman
+      pkgs.fennel-ls
+      pkgs.nil
+      pkgs.fnlfmt
+    ];
     # General Helix settings
     settings = {
       theme = "base16_default";
@@ -23,6 +28,7 @@
     };
     # Configure Language support
     languages = {
+      # Markdowm marksman and nil LSP already configured by default
       language = [
         {
           name = "nix";
@@ -30,8 +36,114 @@
           auto-format = true;
           formatter = { command = "nixpkgs-fmt"; };
         }
-        # Markdowm marksman and nil LSP already configured by default
+        {
+          name = "fennel";
+          scope = "source.fnl";
+          injection-regex = "(fennel|fnl)";
+          file-types = [ "fnl" ];
+          shebangs = [ "fnl" ];
+          comment-tokens = ";";
+          auto-format = false;
+          formatter = { command = "fnlfmt"; args = [ "-" ]; };
+          indent = { tab-width = 2; unit = "  "; };
+          roots = [ ];
+          grammar = "fennel";
+          language-servers = [{ name = "fennel-ls"; }];
+        }
+      ];
+      language-server.fennel-ls = { command = "fennel-ls"; };
+      grammar = [
+        {
+          name = "fennel";
+          source = {
+            git = "https://github.com/travonted/tree-sitter-fennel";
+            rev = "15e4f8c417281768db17080c4447297f8ff5343a";
+          };
+        }
       ];
     };
   };
+
+  xdg.configFile."helix/runtime/queries/fennel/injections.scm".text = '' 
+    ((comment) @injection.content
+      (#set! injection.language "comment"))
+  '';
+  xdg.configFile."helix/runtime/queries/fennel/highlights.scm".text = ''
+      (comment) @comment
+
+    [ "(" ")" "{" "}" "[" "]" ] @punctuation.bracket
+
+    [ ":" ":until" "&" "&as" "?" ] @punctuation.special
+
+    (nil) @constant.builtin
+    (vararg) @punctuation.special
+
+    (boolean) @constant.builtin.boolean
+    (number) @constant.numeric
+
+    (string) @string
+    (escape_sequence) @constant.character.escape
+
+
+    ((symbol) @variable.builtin
+     (#match? @variable.builtin "^[$]"))
+
+    (binding) @symbol
+
+    [ "fn" "lambda" "hashfn" "#" ] @keyword.function
+
+    (fn name: [
+      (symbol) @function
+      (multi_symbol (symbol) @function .)
+    ])
+
+    (lambda name: [
+      (symbol) @function
+      (multi_symbol (symbol) @function .)
+    ])
+
+    (multi_symbol
+      "." @punctuation.delimiter
+      (symbol) @variable.other.member)
+
+    (multi_symbol_method
+      ":" @punctuation.delimiter
+      (symbol) @function.method .)
+
+    [ "for" "each" ] @keyword.control.repeat
+    ((symbol) @keyword.control.repeat
+     (#eq? @keyword.control.repeat
+      "while"))
+
+    [ "match" ] @keyword.control.conditional
+    ((symbol) @keyword.control.conditional
+     (#match? @keyword.control.conditional "^(if|when)$"))
+
+    [ "global" "local" "let" "set" "var" "where" "or" ] @keyword
+    ((symbol) @keyword
+     (#match? @keyword
+      "^(comment|do|doc|eval-compiler|lua|macros|quote|tset|values)$"))
+
+    ((symbol) @keyword.control.import
+     (#match? @keyword.control.import
+      "^(require|require-macros|import-macros|include)$"))
+
+    [ "collect" "icollect" "accumulate" ] @function.macro
+    ((symbol) @function.macro
+     (#match? @function.macro
+      "^(->|->>|-\\?>|-\\?>>|\\?\\.|doto|macro|macrodebug|partial|pick-args|pick-values|with-open)$"))
+
+    ; Lua builtins
+    ((symbol) @constant.builtin
+     (#match? @constant.builtin
+      "^(arg|_ENV|_G|_VERSION)$"))
+
+    ((symbol) @function.builtin
+     (#match? @function.builtin
+      "^(assert|collectgarbage|dofile|error|getmetatable|ipairs|load|loadfile|loadstring|module|next|pairs|pcall|print|rawequal|rawget|rawlen|rawset|require|select|setfenv|setmetatable|tonumber|tostring|type|unpack|warn|xpcall)$"))
+
+    (list . (symbol) @function)
+    (list . (multi_symbol (symbol) @function .))
+    (symbol) @variable
+  '';
 }
