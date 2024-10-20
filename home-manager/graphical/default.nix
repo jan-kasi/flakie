90ac6c6ab35287a34f29e00e4aa07baa88d27ce9
jan-kasi <77466026+jan-kasi@users.noreply.github.com>
2024-10-20 23:32:38 +0100

    sioyek bindings default

--- home-manager/graphical/default.nix
+++ home-manager/graphical/default.nix
@@ -43,14 +43,6 @@
 
   programs.sioyek = {
     enable = true;
-    bindings = {
-      "move_up" = "k";
-      "move_down" = "j";
-      "move_left" = "h";
-      "move_right" = "l";
-      "screen_down" = [ "d" "" ];
-      "screen_up" = [ "u" "" ];
-    };
   };
 
   programs.firefox = {
